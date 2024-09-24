6da1dba54797a792daf46f7291d0610ef6511ef7
rfujii1113 <rfujii@stylesystem.co.jp>
2024-09-24 16:44:45 +0900

    add pakage

--- src/main/java/com/stylesystem/ResumeSystemApplication.java
+++ src/main/java/com/stylesystem/ResumeSystemApplication.java
@@ -1,4 +1,4 @@
-package com.example.postgretest;
+package com.stylesystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/stylesystem/controller/AccountSearchController.java
+++ src/main/java/com/stylesystem/controller/AccountSearchController.java
@@ -1,4 +1,4 @@
-package com.example.postgretest;
+package com.stylesystem.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
