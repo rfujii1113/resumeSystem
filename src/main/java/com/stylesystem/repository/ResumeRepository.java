1365afa107244f9b1bdae15716a07343418ae47e
ottuck <vison901@gmail.com>
2024-10-21 16:41:49 +0900

    skillsWO

--- src/main/java/com/stylesystem/repository/ResumeRepository.java
+++ src/main/java/com/stylesystem/repository/ResumeRepository.java
@@ -1,8 +1,9 @@
 package com.stylesystem.repository;
 
 import com.stylesystem.model.Users;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,6 +11,9 @@ import java.util.Optional;
 @Repository
 public interface ResumeRepository extends JpaRepository<Users, String> {
 
-    @Query("SELECT u FROM Users u LEFT JOIN FETCH u.projects WHERE u.userId = :userId")
-    Optional<Users> findByUserIdWithProjects(String userId);
+    // @Query("SELECT u FROM Users u LEFT JOIN FETCH u.projects WHERE u.userId = :userId")
+    // Optional<Users> findByUserIdWithProjects(@Param("userId") String userId);
+
+    @EntityGraph(attributePaths = "projects")
+    Optional<Users> findByUserId(String userId);
 }
